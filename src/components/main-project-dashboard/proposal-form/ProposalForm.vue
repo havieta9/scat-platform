34cf894b1a6f008a5dd50261b28615f101f21b41
Juan C. Rey <93360398+Kraphene@users.noreply.github.com>
2021-11-15 23:59:11 -0500

    16-11-2021 / create base components

--- src/components/main-project-dashboard/proposal-form/ProposalForm.vue
+++ src/components/main-project-dashboard/proposal-form/ProposalForm.vue
@@ -3,25 +3,134 @@
     <div class="css-proposal-form-swrap">
       <div class="css-proposal-form-wcon">
         <div class="css-proposal-form-title">Submit Cardano Project</div>
+        <div class="css-proposal-form-note">
+          Please fill out the corresponding forms, with the information
+          regarding the project that you want to be audited by the cardano
+          community.
+          The boxes marked with a red asterisk
+          <span class="css-proposal-form-required">*</span> are required and not
+          optional.
+          Only one form can be filled out at a time, once the proposal is
+          submitted and the chain transaction validated.
+          This form should only be filled out in the active proposal stage.
+          Check above the current stage of the audit round.
+        </div>
         <div class="css-proposal-form">
           <form action="" method="post">
             <div class="css-proposal-form-divider">
-              <div><span>Category</span></div>
+              <div>
+                <span
+                  >Category
+                  <span class="css-proposal-form-required">*</span> (e.g. NFT,
+                  DEX, Lending.)</span
+                >
+              </div>
               <input id="category" type="text" name="category" />
             </div>
             <div class="css-proposal-form-divider">
-              <div><span>Name</span></div>
+              <div>
+                <span>Name</span
+                ><span class="css-proposal-form-required">*</span>
+              </div>
               <input id="name" type="text" name="Name" />
             </div>
             <div class="css-proposal-form-divider">
-              <div><span>Description</span></div>
+              <div>
+                <span>Description</span
+                ><span class="css-proposal-form-required">*</span>
+              </div>
               <textarea name="" id="" cols="30" rows="10"></textarea>
             </div>
             <div class="css-proposal-form-divider">
-              <div><span>Tags</span></div>
+              <div>
+                <span>Website</span
+                ><span class="css-proposal-form-required">*</span>
+              </div>
+              <input id="web" type="text" name="web" />
+            </div>
+            <div class="css-proposal-form-divider">
+              <div>
+                <span>Whitepaper</span
+                ><span class="css-proposal-form-required">*</span>
+              </div>
+              <input id="whitepaper" type="text" name="whitepaper" />
+            </div>
+            <div class="css-proposal-form-divider">
+              <div>
+                <span>Token Name</span
+                ><span class="css-proposal-form-required">*</span>
+              </div>
+              <input id="web" type="text" name="web" />
+            </div>
+            <div class="css-proposal-form-divider">
+              <div>
+                <span
+                  >Social Network (Twitter, Subreddit, Telegram, Discord,
+                  others)</span
+                >
+              </div>
+              <input
+                id="socialnetwork1"
+                type="text"
+                name="socialnetwork1"
+                placeholder="..."
+              />
+              <input
+                id="socialnetwork2"
+                type="text"
+                name="socialnetwork2"
+                placeholder="..."
+              />
+              <input
+                id="socialnetwork3"
+                type="text"
+                name="socialnetwork3"
+                placeholder="..."
+              />
+              <input
+                id="socialnetwork4"
+                type="text"
+                name="socialnetwork4"
+                placeholder="..."
+              />
+            </div>
+            <div class="css-proposal-form-divider">
+              <div>
+                <span>Code repository (GitHub, Bitbucket, Gitlab)</span
+                ><span class="css-proposal-form-required">*</span>
+              </div>
               <input id="tags" type="text" name="tags" />
             </div>
-            <input type="submit" value="Save" />
+            <div class="css-proposal-form-divider">
+              <div><span>Catalyst Project Proposal</span></div>
+              <input id="catalyst" type="text" name="catalyst" />
+            </div>
+            <div class="css-proposal-form-divider">
+              <div>
+                <span>Why should this project be audited by the community?</span
+                ><span class="css-proposal-form-required">*</span>
+              </div>
+              <textarea name="why" id="why" cols="30" rows="10"></textarea>
+            </div>
+            <div class="css-proposal-form-divider">
+              <div>
+                <span>Tags</span
+                ><span class="css-proposal-form-required">*</span>
+              </div>
+              <input id="tags" type="text" name="tags" />
+            </div>
+            <div
+              class="css-proposal-form-divider"
+              id="css-proposal-button-divider"
+            >
+              <button class="css-proposal-form-button">Save draft</button>
+              <button
+                class="css-proposal-form-button"
+                id="css-proposal-submit-button"
+              >
+                Submit Proposal
+              </button>
+            </div>
           </form>
         </div>
       </div>
