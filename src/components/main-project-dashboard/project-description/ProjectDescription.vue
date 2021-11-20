3d8f71c26f88584dcadb65ead993cf7d28d5b645
Juan C. Rey <93360398+Kraphene@users.noreply.github.com>
2021-11-20 11:41:35 -0500

    20-11-2021 / create base components

--- src/components/main-project-dashboard/project-description/ProjectDescription.vue
+++ src/components/main-project-dashboard/project-description/ProjectDescription.vue
@@ -1,40 +1,172 @@
 <template>
   <div class="css-project-description-wrap">
-    <div class="css-project-description-swrap">
+    <div class="css-project-description-sub">
       <div class="css-project-description-flex">
-        <div class="css-project-description-head">
-          <button
-            class="css-project-description-close"
-            @click="closeElementDescription()"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 1024 1024"
-              data-v-394d1fd8=""
-              width="24"
-              height="24"
-            >
-              <path
-                fill="currentColor"
-                d="M764.288 214.592 512 466.88 259.712 214.592a31.936 31.936 0 0 0-45.12 45.12L466.752 512 214.528 764.224a31.936 31.936 0 1 0 45.12 45.184L512 557.184l252.288 252.288a31.936 31.936 0 0 0 45.12-45.12L557.12 512.064l252.288-252.352a31.936 31.936 0 1 0-45.12-45.184z"
-              ></path>
-            </svg>
-          </button>
-        </div>
-        <div class="css-project-description-title">
-          <span>SundaeSwap</span>
-        </div>
-        <div class="css-project-description-subtitle">
-          <span>Description</span>
-        </div>
-        <div class="css-project-description-des">
-          SundaeSwap is a decentralized exchange protocol for the Cardano
-          Network, enabling native token and ADA exchanges for anyone. With its
-          protocol, you will be able to swap, stake, lend, borrow, and more in a
-          truly decentralized way!
-        </div>
-        <div class="css-project-description-subtitle">
-          <span>Tags</span>
+        <div class="css-project-description-two">
+          <div class="css-project-description-cl">
+            <div class="css-project-description-cw">
+              <div class="css-project-description-ct">
+                <span>Exchange</span>
+              </div>
+            </div>
+            <div class="css-project-description-hd">
+              <div class="css-project-description-ht">
+                <div class="css-project-description-title">
+                  <span>SundaeSwap</span>
+                </div>
+                <div class="css-project-description-web">
+                  <a href="">www.sundaeswap.com</a>
+                </div>
+              </div>
+              <div class="css-project-description-cp">
+                <div>&#128079;</div>
+                <span>13</span>
+              </div>
+            </div>
+            <div class="css-project-description-subtitle">
+              <span>Description</span>
+            </div>
+            <div class="css-project-description-des">
+              <span
+                >SundaeSwap is a decentralized exchange protocol for the Cardano
+                Network, enabling native token and ADA exchanges for anyone.
+                With its protocol, you will be able to swap, stake, lend,
+                borrow, and more in a truly decentralized way!</span
+              >
+            </div>
+            <div class="css-project-description-subtitle">
+              <span>Why should this project be audited by the community?</span>
+            </div>
+            <div class="css-project-description-des">
+              It is one of the most anticipated exchanges by the cardano
+              community, possibly covering a large market share of native
+              tokens, therefore it is important to know everything about this
+              project.
+            </div>
+            <div class="css-project-description-subtitle">
+              <span>Token Information</span>
+            </div>
+            <div class="css-project-description-des">Swap use case</div>
+
+            <div class="css-project-description-subtitle">
+              <span>Tags</span>
+            </div>
+            <div class="css-project-card-tag-w">
+              <span class="css-project-card-tag"> Dao </span>
+              <span class="css-project-card-tag"> Audit </span>
+              <span class="css-project-card-tag"> Ecosystem </span>
+              <span class="css-project-card-tag"> Dex </span>
+            </div>
+          </div>
+          <div class="css-project-description-cr">
+            <div class="css-project-description-sc">
+              <button
+                class="css-project-description-close"
+                @click="closeElementDescription()"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 1024 1024"
+                  data-v-394d1fd8=""
+                >
+                  <path
+                    fill="var(--base-color-black-primary)"
+                    d="m679.872 348.8-301.76 188.608a127.808 127.808 0 0 1 5.12 52.16l279.936 104.96a128 128 0 1 1-22.464 59.904l-279.872-104.96a128 128 0 1 1-16.64-166.272l301.696-188.608a128 128 0 1 1 33.92 54.272z"
+                  ></path>
+                </svg>
+              </button>
+              <button
+                class="css-project-description-close"
+                @click="closeElementDescription()"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 1024 1024"
+                  data-v-394d1fd8=""
+                  width="24"
+                  height="24"
+                >
+                  <path
+                    fill="var(--base-color-black-primary)"
+                    d="M764.288 214.592 512 466.88 259.712 214.592a31.936 31.936 0 0 0-45.12 45.12L466.752 512 214.528 764.224a31.936 31.936 0 1 0 45.12 45.184L512 557.184l252.288 252.288a31.936 31.936 0 0 0 45.12-45.12L557.12 512.064l252.288-252.352a31.936 31.936 0 1 0-45.12-45.184z"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+            <div class="css-project-description-l">
+              <img src="./sudae.png" alt="" />
+            </div>
+            <div class="css-project-description-subtitle">
+              <span>Token Name</span>
+            </div>
+            <div class="css-project-description-des">SUNDAE</div>
+            <div class="css-project-description-subtitle">
+              <span>Whitepaper</span>
+            </div>
+            <div class="css-project-description-des">
+              <a class="css-project-description-icon">
+                <svg viewBox="0 0 1024 1024" width="24" data-v-394d1fd8="">
+                  <path
+                    fill="currentColor"
+                    d="M715.648 625.152 670.4 579.904l90.496-90.56c75.008-74.944 85.12-186.368 22.656-248.896-62.528-62.464-173.952-52.352-248.96 22.656L444.16 353.6l-45.248-45.248 90.496-90.496c100.032-99.968 251.968-110.08 339.456-22.656 87.488 87.488 77.312 239.424-22.656 339.456l-90.496 90.496zm-90.496 90.496-90.496 90.496C434.624 906.112 282.688 916.224 195.2 828.8c-87.488-87.488-77.312-239.424 22.656-339.456l90.496-90.496 45.248 45.248-90.496 90.56c-75.008 74.944-85.12 186.368-22.656 248.896 62.528 62.464 173.952 52.352 248.96-22.656l90.496-90.496 45.248 45.248zm0-362.048 45.248 45.248L398.848 670.4 353.6 625.152 625.152 353.6z"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+            <div class="css-project-description-subtitle">
+              <span>Repository</span>
+            </div>
+            <div class="css-project-description-red">
+              <a class="css-project-description-icon">
+                <svg
+                  height="24"
+                  aria-hidden="true"
+                  viewBox="0 0 16 16"
+                  version="1.1"
+                  width="32"
+                  class=""
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+            <div class="css-project-description-subtitle">
+              <span>Social Networks</span>
+            </div>
+            <div class="css-project-description-red">
+              <a class="css-project-description-icon">
+                <svg width="24" viewBox="0 0 24 24" aria-hidden="true" class="">
+                  <g>
+                    <path
+                      fill="#1d9bf0"
+                      d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"
+                    ></path>
+                  </g>
+                </svg>
+              </a>
+              <a class="css-project-description-icon">
+                <svg
+                  width="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  class=""
+                >
+                  <g>
+                    <circle fill="#FF4500" cx="10" cy="10" r="10"></circle>
+                    <path
+                      fill="#FFF"
+                      d="M16.67,10A1.46,1.46,0,0,0,14.2,9a7.12,7.12,0,0,0-3.85-1.23L11,4.65,13.14,5.1a1,1,0,1,0,.13-0.61L10.82,4a0.31,0.31,0,0,0-.37.24L9.71,7.71a7.14,7.14,0,0,0-3.9,1.23A1.46,1.46,0,1,0,4.2,11.33a2.87,2.87,0,0,0,0,.44c0,2.24,2.61,4.06,5.83,4.06s5.83-1.82,5.83-4.06a2.87,2.87,0,0,0,0-.44A1.46,1.46,0,0,0,16.67,10Zm-10,1a1,1,0,1,1,1,1A1,1,0,0,1,6.67,11Zm5.81,2.75a3.84,3.84,0,0,1-2.47.77,3.84,3.84,0,0,1-2.47-.77,0.27,0.27,0,0,1,.38-0.38A3.27,3.27,0,0,0,10,14a3.28,3.28,0,0,0,2.09-.61A0.27,0.27,0,1,1,12.48,13.79Zm-0.18-1.71a1,1,0,1,1,1-1A1,1,0,0,1,12.29,12.08Z"
+                    ></path>
+                  </g>
+                </svg>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
       <div class="css-project-description-pub">
@@ -89,16 +221,19 @@
           </div>
           <div class="css-project-description-pub-cr">
             <div class="css-project-description-pub-crn">
-              Luis Hoskinson <span>2 hours ago</span>
+              Andrea Hoskinson <span>2 hours ago</span>
             </div>
             <div class="css-project-description-pub-crc">
               I think this project is very promising in the cardano ecosystem, I
               am an investor and I would like to know the result of this audit.
+              It is one of the most anticipated exchanges by the cardano
+              community, possibly covering a large market share of native
+              tokens, therefore it is important to know everything about this
+              project.
             </div>
           </div>
         </div>
       </div>
-      <div></div>
     </div>
   </div>
 </template>
