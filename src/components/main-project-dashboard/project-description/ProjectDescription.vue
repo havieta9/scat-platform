9dc0ba663c85773e2256d5236ac8946ebb364f55
Juan C. Rey <93360398+Kraphene@users.noreply.github.com>
2021-11-20 23:35:54 -0500

    20-11-2021 / create base components

--- src/components/main-project-dashboard/project-description/ProjectDescription.vue
+++ src/components/main-project-dashboard/project-description/ProjectDescription.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="css-project-description-wrap">
     <div class="css-project-description-sub">
-      <div class="css-project-description-flex">
+      <div class="css-project-description-flx">
         <div class="css-project-description-two">
           <div class="css-project-description-cl">
             <div class="css-project-description-cw">
@@ -101,11 +101,13 @@
             <div class="css-project-description-subtitle">
               <span>Token Name</span>
             </div>
-            <div class="css-project-description-des">SUNDAE</div>
+            <div class="css-project-description-rd">
+              <span>SUNDAE</span>
+            </div>
             <div class="css-project-description-subtitle">
               <span>Whitepaper</span>
             </div>
-            <div class="css-project-description-des">
+            <div class="css-project-description-rd">
               <a class="css-project-description-icon">
                 <svg viewBox="0 0 1024 1024" width="24" data-v-394d1fd8="">
                   <path
@@ -118,7 +120,7 @@
             <div class="css-project-description-subtitle">
               <span>Repository</span>
             </div>
-            <div class="css-project-description-red">
+            <div class="css-project-description-rd">
               <a class="css-project-description-icon">
                 <svg
                   height="24"
@@ -138,7 +140,7 @@
             <div class="css-project-description-subtitle">
               <span>Social Networks</span>
             </div>
-            <div class="css-project-description-red">
+            <div class="css-project-description-rd">
               <a class="css-project-description-icon">
                 <svg width="24" viewBox="0 0 24 24" aria-hidden="true" class="">
                   <g>
@@ -170,9 +172,37 @@
         </div>
       </div>
       <div class="css-project-description-pub">
-        <div class="css-project-description-pub-t">Public discussion</div>
+        <div class="css-project-description-pub-t">
+          <div>Public discussion</div>
+          <div class="css-project-description-cb">
+            <span>1</span>
+            <span>Comments</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1024 1024"
+              data-v-394d1fd8=""
+              width="20"
+              style="transform: scaleX(-1); opacity: 0.5"
+            >
+              <path
+                d="m174.72 855.68 135.296-45.12 23.68 11.84C388.096 849.536 448.576 864 512 864c211.84 0 384-166.784 384-352S723.84 160 512 160 128 326.784 128 512c0 69.12 24.96 139.264 70.848 199.232l22.08 28.8-46.272 115.584zm-45.248 82.56A32 32 0 0 1 89.6 896l58.368-145.92C94.72 680.32 64 596.864 64 512 64 299.904 256 96 512 96s448 203.904 448 416-192 416-448 416a461.056 461.056 0 0 1-206.912-48.384l-175.616 58.56z"
+              ></path>
+              <path
+                d="M512 563.2a51.2 51.2 0 1 1 0-102.4 51.2 51.2 0 0 1 0 102.4zm192 0a51.2 51.2 0 1 1 0-102.4 51.2 51.2 0 0 1 0 102.4zm-384 0a51.2 51.2 0 1 1 0-102.4 51.2 51.2 0 0 1 0 102.4z"
+              ></path>
+            </svg>
+          </div>
+        </div>
         <div class="css-project-description-pub-i">
-          <div class="css-project-description-pub-p"></div>
+          <div class="css-project-description-pub-p">
+            <img
+              width="39"
+              height="39"
+              style="border-radius: 50%"
+              src="https://i.stack.imgur.com/frlIf.png"
+              alt=""
+            />
+          </div>
           <input
             class="css-project-description-pub-c"
             id="comment"
@@ -181,6 +211,7 @@
             placeholder="write a comment..."
           />
         </div>
+
         <div class="css-project-description-pub-l">
           <div class="css-project-description-pub-v">
             <div class="css-project-description-pub-vf">
@@ -221,7 +252,7 @@
           </div>
           <div class="css-project-description-pub-cr">
             <div class="css-project-description-pub-crn">
-              Andrea Hoskinson <span>2 hours ago</span>
+              Charles Hoskinson <span>2 hours ago</span>
             </div>
             <div class="css-project-description-pub-crc">
               I think this project is very promising in the cardano ecosystem, I
@@ -230,6 +261,84 @@
               community, possibly covering a large market share of native
               tokens, therefore it is important to know everything about this
               project.
+            </div>
+            <div class="css-project-description-pub-rpl">
+              <div>
+                <svg class="svg-icon" viewBox="0 0 20 20" width="20">
+                  <path
+                    fill="var(--complementary-color-blue)"
+                    d="M16.198,10.896c-0.252,0-0.455,0.203-0.455,0.455v2.396c0,0.626-0.511,1.137-1.138,1.137H5.117c-0.627,0-1.138-0.511-1.138-1.137V7.852c0-0.626,0.511-1.137,1.138-1.137h5.315c0.252,0,0.456-0.203,0.456-0.455c0-0.251-0.204-0.455-0.456-0.455H5.117c-1.129,0-2.049,0.918-2.049,2.047v5.894c0,1.129,0.92,2.048,2.049,2.048h9.488c1.129,0,2.048-0.919,2.048-2.048v-2.396C16.653,11.099,16.45,10.896,16.198,10.896z"
+                  ></path>
+                  <path
+                    fill="var(--complementary-color-blue)"
+                    d="M14.053,4.279c-0.207-0.135-0.492-0.079-0.63,0.133c-0.137,0.211-0.077,0.493,0.134,0.63l1.65,1.073c-4.115,0.62-5.705,4.891-5.774,5.082c-0.084,0.236,0.038,0.495,0.274,0.581c0.052,0.019,0.103,0.027,0.154,0.027c0.186,0,0.361-0.115,0.429-0.301c0.014-0.042,1.538-4.023,5.238-4.482l-1.172,1.799c-0.137,0.21-0.077,0.492,0.134,0.629c0.076,0.05,0.163,0.074,0.248,0.074c0.148,0,0.294-0.073,0.382-0.207l1.738-2.671c0.066-0.101,0.09-0.224,0.064-0.343c-0.025-0.118-0.096-0.221-0.197-0.287L14.053,4.279z"
+                  ></path>
+                </svg>
+              </div>
+              <span>Reply</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="css-project-description-pub-l">
+          <div class="css-project-description-pub-v">
+            <div class="css-project-description-pub-vf">
+              <span>
+                <button
+                  class="css-project-description-pub-vfa"
+                  style="transform: scaleY(-1)"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 1024 1024"
+                    data-v-394d1fd8=""
+                    width="20"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M544 805.888V168a32 32 0 1 0-64 0v637.888L246.656 557.952a30.72 30.72 0 0 0-45.312 0 35.52 35.52 0 0 0 0 48.064l288 306.048a30.72 30.72 0 0 0 45.312 0l288-306.048a35.52 35.52 0 0 0 0-48 30.72 30.72 0 0 0-45.312 0L544 805.824z"
+                    ></path>
+                  </svg></button
+              ></span>
+              <span>5</span>
+              <span>
+                <button class="css-project-description-pub-vfa">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 1024 1024"
+                    data-v-394d1fd8=""
+                    width="20"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M544 805.888V168a32 32 0 1 0-64 0v637.888L246.656 557.952a30.72 30.72 0 0 0-45.312 0 35.52 35.52 0 0 0 0 48.064l288 306.048a30.72 30.72 0 0 0 45.312 0l288-306.048a35.52 35.52 0 0 0 0-48 30.72 30.72 0 0 0-45.312 0L544 805.824z"
+                    ></path>
+                  </svg>
+                </button>
+              </span>
+            </div>
+          </div>
+          <div class="css-project-description-pub-cr">
+            <div class="css-project-description-pub-crn">
+              Bullish Dumpling <span>4 hours ago</span>
+            </div>
+            <div class="css-project-description-pub-crc">
+              I hope to see the result of this project!
+            </div>
+            <div class="css-project-description-pub-rpl">
+              <div>
+                <svg class="svg-icon" viewBox="0 0 20 20" width="20">
+                  <path
+                    fill="var(--complementary-color-blue)"
+                    d="M16.198,10.896c-0.252,0-0.455,0.203-0.455,0.455v2.396c0,0.626-0.511,1.137-1.138,1.137H5.117c-0.627,0-1.138-0.511-1.138-1.137V7.852c0-0.626,0.511-1.137,1.138-1.137h5.315c0.252,0,0.456-0.203,0.456-0.455c0-0.251-0.204-0.455-0.456-0.455H5.117c-1.129,0-2.049,0.918-2.049,2.047v5.894c0,1.129,0.92,2.048,2.049,2.048h9.488c1.129,0,2.048-0.919,2.048-2.048v-2.396C16.653,11.099,16.45,10.896,16.198,10.896z"
+                  ></path>
+                  <path
+                    fill="var(--complementary-color-blue)"
+                    d="M14.053,4.279c-0.207-0.135-0.492-0.079-0.63,0.133c-0.137,0.211-0.077,0.493,0.134,0.63l1.65,1.073c-4.115,0.62-5.705,4.891-5.774,5.082c-0.084,0.236,0.038,0.495,0.274,0.581c0.052,0.019,0.103,0.027,0.154,0.027c0.186,0,0.361-0.115,0.429-0.301c0.014-0.042,1.538-4.023,5.238-4.482l-1.172,1.799c-0.137,0.21-0.077,0.492,0.134,0.629c0.076,0.05,0.163,0.074,0.248,0.074c0.148,0,0.294-0.073,0.382-0.207l1.738-2.671c0.066-0.101,0.09-0.224,0.064-0.343c-0.025-0.118-0.096-0.221-0.197-0.287L14.053,4.279z"
+                  ></path>
+                </svg>
+              </div>
+              <span>Reply</span>
             </div>
           </div>
         </div>
